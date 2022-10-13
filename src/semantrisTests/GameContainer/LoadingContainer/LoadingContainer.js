3c9baa3a49548a6526a25064b3ea0a2e7e47b947
Bilal Abbas <babbas0505@gmail.com>
2022-10-13 15:24:51 +0100

    centering

--- src/semantrisTests/GameContainer/LoadingContainer/LoadingContainer.js
+++ src/semantrisTests/GameContainer/LoadingContainer/LoadingContainer.js
@@ -1,7 +1,6 @@
 // import '@pixi/gif';
 import * as PIXI from "pixi.js";
-import '@pixi/gif';
-import img from "../../img/sunspritesheet.png";
+// import img from "../../img/sunspritesheet.png";
 import img1 from "../../img/sun_spritesheets/frame_00_delay-0.06s.gif"
 import img2 from "../../img/sun_spritesheets/frame_01_delay-0.06s.gif"
 import img3 from "../../img/sun_spritesheets/frame_02_delay-0.06s.gif"
@@ -23,54 +22,43 @@ import img18 from "../../img/sun_spritesheets/frame_17_delay-0.06s.gif"
 import img19 from "../../img/sun_spritesheets/frame_18_delay-0.06s.gif"
 import img20 from "../../img/sun_spritesheets/frame_19_delay-0.06s.gif"
 
+import sunSprite from  "../../img/sun_spritesheets/suns-sp.json"
+
 import { gsap } from "gsap";
 import { PixiPlugin } from "gsap/PixiPlugin";
 gsap.registerPlugin(PixiPlugin);
-
 export class LoadingContainer extends PIXI.Container {
   constructor(parent = null) {
     super();
     this.parent = parent;
     this.position.set(window.innerWidth / 2, window.innerHeight / 2)
 
+    
     const arr = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10, img11, img12, img13, img14, img15, img16, img17, img18, img19, img20]
     let textureArray = []
-
-    const arr2 = ["img/sun_spritesheets/frame_00_delay-0.06s.gif"]
-
-    console.log('>>', arr[0], arr2[0])
-
     for (let i = 0; i < arr.length; i++) {
       let texture = PIXI.Texture.from(arr[i]);
       textureArray.push(texture);
     }
-
     let animatedSprite = new PIXI.AnimatedSprite(textureArray);
-    // animatedSprite.position.set(0.5, 1)
+    animatedSprite.position.set(-200, -150)
     animatedSprite.play()
     animatedSprite.animationSpeed = 0.4
     this.addChild(animatedSprite)
-    // console.log(img1)
   
-    if (this.parent) {
-      console.log(this.parent, 'running')
-      this.parent.addChild(this);
-      this.position.x = window.innerWidth / 2;
-      this.position.y = (window.innerHeight / 2) - 5;
-    }
-
+    // if (this.parent) {
+    //   this.parent.addChild(this);
+    //   this.position.x = window.innerWidth / 2;
+    //   this.position.y = (window.innerHeight / 2) - 5;
+    // }
     const loadingText = new PIXI.Text("Loading...", {
-      fontFamily: "Arial",
+      fontFamily: "Chakra Petch",
       fontSize: 24,
-      fill: 0xffffff,
-      align: "center",
+      fill: 0x000000,
+      // align: "center",
     })
-    loadingText.position.set(0.5, 1)
+    console.log(window.innerWidth / 2)
+    loadingText.position.set(-5, -50)
     this.addChild(loadingText)
   }
-  // async loadGif() {
-  //   console.log(Assets)
-  //   const image = await PIXI.Assets.load('img/sun.gif');
-  //   return image
-  // }
 }
